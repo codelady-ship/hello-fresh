7f39643f0520c3ceeefaca04279fe068e8be4e49
codelady-ship <jalilova.antiqa@gmail.com>
2024-11-29 19:11:58 +0400

    hello fresh

---
+++ numune.js
@@ -0,0 +1,110 @@
+//1ci ders
+       /* let variable1 = 3;
+        console.log(variable1);
+    
+        let calculation = 2 + 2;
+        console.log(calculation);
+        console.log(calculation+2);
+        let result=calculation +2 ;
+        console.log(result);
+    
+        let message='salam';
+        console.log(message);
+        console.log(';');
+    
+    let variable4 = 5;// deyer 5 olacaq
+    console.log(variable4);
+    
+    variable4=variable4+1;
+    console.log(variable4);//artiq deyer 5+1=6 olacaq
+    
+    const variable2=3;
+    //variable2=5;//deyer deyismeyecek error verecek.deyismek istediyimiz deyer ucunlet qalanina const isletmek yaxsidi
+    var variable3=3;
+    console.log(typeof variable3);
+    console.log(typeof 3);
+    console.log(typeof message);//string yazilacaq
+
+      /*let name='antiqa';
+      console.log(name);
+      console.log(`my name is:${name}`);
+      const calculation = 5+2*3+9;
+      console.log(calculation);
+      console.log(`cost of food:$${calculation}`);
+      const result= calculation +calculation*0.1
+      console.log(result);
+      console.log(`tax 10%:$${result}`);*/
+
+//2ci ders
+   /* true 
+    false
+    console.log(8>3);
+    console.log(8<3);
+    console.log(8<3*2);
+    console.log(typeof true);
+    console.log(5== '5');
+
+
+  if(false){
+    console.log('hello')}else{console.log('else');
+    }
+    if(true){
+    console.log('hello')}else{console.log('else');
+   }
+
+   const age=15
+   if(age>=16){console.log('You can drive')}
+   else if(age>=14){console.log('You can not drive');}
+   else if(age>=14){'Almost there'}
+   
+   //and operators
+ console.log(true&&true);
+   console.log(true&&false);
+   console.log(0.2>=0 && 0.2< 1/3);
+
+   //or operators
+  console.log(true || true);
+   console.log(!true);
+   console.log(!false);
+   
+   //truth false 
+   //false values-null.undefined.infinity.false.0."".NaN
+   const cartQuantity=5;
+  if(cartQuantity){console.log("cart has product")};
+  console.log(!0);//true
+  console.log("text"/5);//nan
+  //let variable1;
+  //console.log(variable1);  //undefined
+  //cost variable1 = undefined;
+  //console.log(variable1);
+  //const result = true ? 'truthy' : 'falsy';
+  /*console.log(result);
+false && console.log(' hello')//false
+const message =false && 'hello';
+console.log(message);//false
+const message = 6 && 'hello';
+console.log(message);//true*/
+/*const currency =  'EUR' || 'USD'
+console.log(currency);
+//const currency =  undefined || 'USD'
+//console.log(currency);*/
+
+/*document.getElementById("myH1").textContent= 'hello'
+let myName="antiqa"
+let age = 25
+document.getElementById("myP").textContent= `my name is ${myName}.I am ${age} years old`
+
+// input yazdirmaq:islemedi>>>>>>>>>>>>>>>>>>
+let username;
+
+document.getElementById("mySubmit").onclick=function(){
+  username=document.getElementById("myText").value;
+  console.log(username)
+}
+*/
+/*let ad;
+
+document.getElementById("mySubmit").onclick = function(){
+  ad=document.getElementById("myText").value;
+  document.getEelementById("myH1").textContent = `salam ${ad}`
+}*/
